cca0d71052b2e5b28b0f0a048df4501d5742e681
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-08-03 14:50:15 +0300

    adding selectors for selecting html tags

---
+++ static/script.js
@@ -0,0 +1,5 @@
+const header = document.getElementById("header"),
+  navMenu = document.getElementById("nav-menu"),
+  navOpen = document.getElementById("nav-open"),
+  navClose = document.getElementById("nav-close"),
+  navLinks = document.querySelectorAll(".nav__link");
