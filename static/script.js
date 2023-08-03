f4e8b7a35d0967da3ac8e93a77d61719d9bc7129
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-08-03 14:50:47 +0300

    adding navbar functionality

--- static/script.js
+++ static/script.js
@@ -3,3 +3,33 @@ const header = document.getElementById("header"),
   navOpen = document.getElementById("nav-open"),
   navClose = document.getElementById("nav-close"),
   navLinks = document.querySelectorAll(".nav__link");
+
+/* Change header style on scroll
+---------------------------------------*/
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 80) {
+    header.classList.add("header--scroll");
+  } else {
+    header.classList.remove("header--scroll");
+  }
+});
+
+/* Navigation Menu
+---------------------------------------*/
+
+// Open the nav menu
+navOpen.addEventListener("click", () => {
+  navMenu.classList.add("nav__menu--open");
+});
+
+// Close the nav menu
+navClose.addEventListener("click", () => {
+  navMenu.classList.remove("nav__menu--open");
+});
+
+// Close the nav menu when the user clicks on each nav link
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("nav__menu--open");
+  });
+});
